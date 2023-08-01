2f2cb3868c5700438f8c591c7e70d89500ecfa90
Полина Тригуб PD-87 <doc21@inbox.ru>
2023-08-01 15:39:55 +0300

    task 3

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ class Student:
         else:
             return 'Ошибка'
 
+    def _count_rate_st(self, grade):
+        for grades in self.grades:
+            value = grades.value()
+            average = sum(value)/len(value)
+
+    def __lt__(self):
+        def _count_rate_st(self, grade)
+            return self
+
+    print(f'Имя: {self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции: {self._count_rate_st}\nКурсы в процессе изучения:{" ".join(self.courses_in_progress)}\nЗавершенные курсы:{" ".join(self.finished_courses)})
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,10 +40,22 @@ class Lecturer(Mentor):
         self.surname = surname
         self.lec = {}
 
-
+    def _count_rate(self, grade):
+        for grades in self.lec:
+            value = grades.value()
+            new_grade = sum(value)/len(value)
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции: {self._count_rate}'
+        return res
+    def __lt__(self):
+        def _count_rate(self, grade):
+            return self
 
 
 class Reviewer(Mentor):
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия:{self.surname}'
+        return res
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -44,6 +66,9 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
+some_reviewer = Reviewer("Some", "Buddy")
+some_lecturer = Lecturer("Some", "Buddy", ) #Не пойму, как вписать сюда значения оценок за лекции
+some_student = Student('Ruoy', 'Eman', 'man', 'Введение в программирование', 'Python, Git') #И сюда тоже :(
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
